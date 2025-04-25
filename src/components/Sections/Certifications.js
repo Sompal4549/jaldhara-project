087187ad472d98b063a6498ee65d9a7cd2e3a142
sompal <sompal7678@gmail.com>
2025-04-25 13:09:28 +0530

    fix: slider anc certifications

--- src/components/Sections/Certifications.js
+++ src/components/Sections/Certifications.js
@@ -4,35 +4,41 @@ import Image from "next/image";
 const Certifications = () => {
   return (
     <div className="w-full py-4 px-4 bg-primary" id="certifications">
-      <SectionTitle text="Our Certifications" />
-      <div className="items-center flex justify-center gap-10 flex-wrap">
-        {Array.from({ length: 3 }).map((_, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center w-[200px] text-center"
-            >
-              <div className="w-[200px] h-[200px] bg-white/10 rounded-full flex items-center text-center justify-center">
-                <Image
-                  src="./certification.svg"
-                  alt="certification icon"
-                  width={110}
-                  height={110}
-                  className=""
-                />
+      <div className="max-w-[1200px] w-[100%] mx-auto">
+        <div className="w-full py-8">
+          <SectionTitle text="Our Certifications" />
+        </div>
+        <div className="items-center flex gap-10 flex-wrap w-full justify-around py-8">
+          {Array.from({ length: 3 }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center w-[250px] text-center gap-4"
+              >
+                <div className="w-[200px] h-[200px] bg-white/10 rounded-full flex items-center text-center justify-center">
+                  <Image
+                    src="./certification.svg"
+                    alt="certification icon"
+                    width={110}
+                    height={110}
+                    className=""
+                  />
+                </div>
+                <div className="px-4 text-white">
+                  <p className="font-inter-sans text-sm">
+                    BIS Certification (BIS 4984)
+                  </p>
+                  <p className="font-inter-sans text-2xs leading-sm">
+                    CM/L-NO: 5800024409
+                  </p>
+                  <p className="font-inter-sans text-2xs leading-sm">
+                    Issue Date : 30 Aug 2018
+                  </p>
+                </div>
               </div>
-              <div className="px-4 text-white">
-                <p className="font-inter-sans text-sm">
-                  BIS Certification (BIS 4984)
-                </p>
-                <p className="font-inter-sans text-2xs">CM/L-NO: 5800024409</p>
-                <p className="font-inter-sans text-2xs">
-                  Issue Date : 30 Aug 2018
-                </p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Sections/WeDeal.js
+++ src/components/Sections/WeDeal.js
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import SectionTitle from "../Headings/SectionTitle";
-import DealSlider from "@/components/Sliders/DealSlider";
+// import DealSlider from "@/components/Sliders/DealSlider";
+import Image from "next/image";
 const WeDeal = () => {
   const [index, setIndex] = useState(0);
   const clickHandler = (index) => {
@@ -15,16 +16,70 @@ const WeDeal = () => {
   return (
     <div className="w-full py-4 px-4 bg-primary" id="we-deal">
       <SectionTitle text="Industries we deal with" />
-      <DealSlider index={index} />
+      <div
+        className={`flex transition-transform duration-700 ease-in-out`}
+        style={{ transform: `translateX(-${index * 100}%)` }}
+      >
+        {Array.from({ length: 5 }).map((_, ind) => (
+          <div
+            key={ind}
+            // className={`w-full h-[720px] rounded-s-lg bg-white ${
+            //   ind === index ? "block" : "hidden"
+            // }`}
+            className="min-w-full h-[720px] flex flex-row  rounded-s-lg px-4"
+          >
+            <div className="flex flex-row bg-white rounded-lg">
+              <div className="img-container w-[55%] bg-blend-multiply rounded-lg">
+                <Image
+                  alt="our work"
+                  width={750}
+                  height={720}
+                  className="w-auto h-full max-h-[720px] bg-primary rounded-s-lg"
+                  src="./construction.svg"
+                />
+              </div>
+              <div className="w-[45%] pl-8 py-8">
+                <p className="text-sm font-inter-sans font-bold leading-xs text-dark">
+                  Construction {ind}
+                </p>
+                <ul className="font-manrope-sans font-light text-2xs tracking-normal list-disc pl-5">
+                  <li>
+                    <p className="mt-5 text-[rgba(0, 19, 37, 0.85)]">
+                      Water Supply Systems: HDPE pipes are ideal for
+                      transporting drinking water due to their resistance to
+                      corrosion and chemicals, ensuring safe and reliable water
+                      distribution.
+                    </p>
+                  </li>
+                  <li>
+                    <p className="mt-5 text-[rgba(0, 19, 37, 0.85)]">
+                      Drainage Systems: Used in sewage and drainage systems
+                      because of their smooth inner surface, which reduces
+                      blockages and ensures efficient waste flow.
+                    </p>
+                  </li>
+                  <li>
+                    <p className="mt-5 text-[rgba(0, 19, 37, 0.85)]">
+                      Geotechnical Applications: Utilized in soil stabilization
+                      projects due to their flexibility and resistance to
+                      environmental factors.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <ul className="bg-primary flex gap-2 cursor-pointer my-5 w-full items-center justify-center">
         {Array.from({ length: 5 }).map((_, ind) => (
           <li
-            key={`dot-${index}-item`}
+            key={`dot-${ind}-item`}
             className={`p-1 h-2 ${
               ind === index ? "w-6 bg-secondary" : "w-3 bg-white"
             } rounded-full`}
-            onClick={() => clickHandler(index)}
+            onClick={() => clickHandler(ind)}
           ></li>
         ))}
       </ul>
