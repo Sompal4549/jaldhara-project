02749e9f707e57fa2c7e67e9eb48afe16b2038ab
sompal <sompal7678@gmail.com>
2025-04-28 11:10:09 +0530

    feat: about page seo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,12 @@
 import HomeSection from "@/components/Sections/HomeSection";
 import PageLayout from "@/components/Ui/PageLayout";
 import AboutUs from "@/components/Sections/AboutPage/AboutUs"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Jaldhara Polytubes",
+  description: "We are pioneering the future of piping solutions in an era defined by precision, sustainability, and innovation. Our approach blends cutting-edge technology, eco-conscious practices, and unparalleled durability to shape the next generation of infrastructure.",
+};
 
 export default function About() {
   const bannerData = {
